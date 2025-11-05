1b09e8c5d39758240a210eced67b6fb007b4a9a7
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-11-05 12:04:10 +0530

    Update backend

--- BACKEND/UserMS/src/main/java/com/hms_backend/UserMS/jwt/JwtUtil.java
+++ BACKEND/UserMS/src/main/java/com/hms_backend/UserMS/jwt/JwtUtil.java
@@ -1,20 +1,32 @@
 package com.hms_backend.UserMS.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class JwtUtil {
 
+    private static final String SECRET_KEY = "1mDusXKpi6ODmTR2Cn94WgRI/moESixrW70gsecbyM/hd1ikRmAzimS2fEwD8tCZG9lfzg+unV8PWXAkQsbNhQ==";
+    private static final Long JWT_TOKEN_VALIDITY = 5 * 60 * 60L;
+
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         CustomUserDetails user = (CustomUserDetails) userDetails;
     }
 
-    public String doGenerateToken(Map<String, Object> claims, String subject) {
-        return null;
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
     }
 }
