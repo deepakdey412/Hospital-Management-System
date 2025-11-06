ae65d857e894745ec7b819e0895ff537d096d199
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-11-07 00:16:36 +0530

    Add cors configuration

---
+++ BACKEND/UserMS/src/main/java/com/hms_backend/UserMS/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.hms_backend.UserMS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
