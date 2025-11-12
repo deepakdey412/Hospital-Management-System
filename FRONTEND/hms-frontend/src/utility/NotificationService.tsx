548dff590fb3185d3e10c93f4bb0b85bc24afe80
Deepak Dey <deepakdey412@gmail.com>
2025-11-12 23:51:40 +0530

    Update NotificationService.tsx

--- FRONTEND/hms-frontend/src/utility/NotificationService.tsx
+++ FRONTEND/hms-frontend/src/utility/NotificationService.tsx
@@ -3,12 +3,6 @@ import {  IconCheck, IconX } from '@tabler/icons-react';
 
 export const successNotification = (message: string) => {
   notifications.show({
-    title: "🎉 Successfully Registered!",
-    message: message,
-    color: 'teal',
-    icon: <IconCheck/>,
-    withCloseButton: true,            // Rounded corners
-    withBorder: true,        // Adds subtle border
-    className : "border-green-500"        // Auto closes after 5 seconds
+
   });
 };
